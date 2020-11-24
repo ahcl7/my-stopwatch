d41cfea7a7f1cdf216c06f32adea7921384d6ddc
long.nguyen2 <long.nguyen2@gigasource.io>
2020-11-24 14:39:51 +0700

    add fonts props for column tag

--- packages/tom/src/tagRenderers/column.ts
+++ packages/tom/src/tagRenderers/column.ts
@@ -1,23 +1,24 @@
 import { createTagRenderer } from './TagRenderer';
 import { Alignment, align } from '../helpers/alignment';
 
+import { applyFont } from '../helpers/font'
 export default createTagRenderer(
   (
     {
       gapSize = 0,
       crossAxisAlignment = 'start',
+       font = "default"
     }: {
       gapSize?: number;
       crossAxisAlignment?: Alignment;
+      font?: string
     },
     { children },
   ) => {
-    return children
-      .map((s) => s.split('\n'))
-      .map((lines) => {
-        const maxLength = Math.max(...children.flatMap((s) => s.split('\n').map((l) => l.length)));
-        return lines.map((line) => align(line, crossAxisAlignment, maxLength, () => ' '));
-      })
+    const fontAppliedChildren = children.map(s => applyFont(s, font))
+    const maxLength = Math.max(...fontAppliedChildren.map((s) => Math.max(...s.split('\n').map(l => l.length))))
+    return fontAppliedChildren.map((text) => {
+       return text.split('\n').map((line) => { return align(line, crossAxisAlignment, maxLength, () => ' ') }).join('\n')})
       .join('\n'.repeat(gapSize + 1));
   },
 );
